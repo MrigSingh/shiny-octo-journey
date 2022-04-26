5687726b3b26e511abba6af444b42cdcc992e061
mrigsingh <singh.mriganshu@gmail.com>
2022-04-26 23:55:02 +0530

    added google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -7,6 +7,17 @@ class MyDocument extends Document {
         <Head>
           <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
           <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-P6SNB8RZ1K" />
+          <script 
+            dangerouslySetInnerHTML={{
+              __html: `
+              window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-P6SNB8RZ1K');
+            `}}
+          />
         </Head>
         <body>
           <Main />
